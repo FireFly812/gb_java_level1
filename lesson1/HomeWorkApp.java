d5d2ebd5b4790e38113a707a86aa2ed195de3d79
s.koropovskaya <s.koropovskaya@argustelecom.ru>
2022-01-13 21:09:19 +0300

    отступы

--- lesson1/HomeWorkApp.java
+++ lesson1/HomeWorkApp.java
@@ -11,39 +11,39 @@ public class HomeWorkApp {
 		compareNumbers();
 	}
 
-	public static void printThreeWords(){
+	public static void printThreeWords() {
 		System.out.println("Orange");
 		System.out.println("Banana");
 		System.out.println("Apple");
 	}
-	public static void checkSumSign(){
-		int a=-2;
-		int b=0;
-		int c = a+b;
-		System.out.println(c<0? "Сумма отрицательная" : "Сумма положительная");
+
+	public static void checkSumSign() {
+		int a = -2;
+		int b = 0;
+		int c = a + b;
+		System.out.println(c < 0 ? "Сумма отрицательная" : "Сумма положительная");
 	}
 
-	public static void printColor(){
-	int value = 101;
+	public static void printColor() {
+		int value = 101;
 		if (value <= 0) {
 			System.out.println("Красный");
 		}
-		if (value >0 && value <=100) {
+		if (value > 0 && value <= 100) {
 			System.out.println("Желтьй");
 		}
-		if (value >100) {
+		if (value > 100) {
 			System.out.println("Зеленый");
 		}
 	}
-	public static void compareNumbers(){
-	int a=20;
-	int b=20;
-		if (a>=b) {
-		System.out.println("a >= b");
-		}
-		else {
+
+	public static void compareNumbers() {
+		int a = 20;
+		int b = 20;
+		if (a >= b) {
+			System.out.println("a >= b");
+		} else {
 			System.out.println("a < b");
 		}
 	}
 }
-
